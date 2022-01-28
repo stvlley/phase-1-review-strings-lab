8cdd75462193adfbbb3c69fbaaacb305ed826d18
stephen talley <stvlley.amzn@gmail.com>
2022-01-28 14:37:30 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+let currentUser = 'Stephen Talley';
+
+//describe welcomeMassage to equal "" with interpolated currentUser
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// make toUpperCase current user
+let upperCaseCurrentUser = currentUser.toUpperCase();
+// essentially the same but everything toUpperCase
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+
+
+//make firstInitial and restOfName
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+// describe shortGreeting to have string "welcome"
+let shortGreeting = `Welcome, ${firstInitial}!`;
+
+console.log(shortGreeting)
